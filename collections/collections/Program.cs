ee150f12043f26499a9aa4e5650db9e6ed3ea940
lin <lindadavies1708@gmail.com>
2023-09-29 09:00:24 +0200

    finished

---
+++ collections/collections/Program.cs
@@ -0,0 +1,108 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ExColletions
+{
+
+    class SchoolClass
+{
+    public string Name { get; }
+    public string ClassTeacher { get; }
+
+    public IReadOnlyList<Student> Students => students.AsReadOnly();
+    private List<Student> students = new List<Student>();
+
+    public SchoolClass(string name, string classTeacher)
+    {
+        Name = name;
+        ClassTeacher = classTeacher;
+    }
+
+    public HashSet<string> Cities
+    {
+        get
+        {
+            HashSet<string> cities = new HashSet<string>();
+            foreach (var student in students)
+            {
+                cities.Add(student.City);
+            }
+            return cities;
+        }
+    }
+
+    public void AddStudent(Student student)
+    {
+        students.Add(student);
+        student.SchoolClass = this;
+    }
+
+    public void RemoveStudent(Student student)
+    {
+        students.Remove(student);
+        student.SchoolClass = null;
+    }
+}
+    class Student
+    {
+        public int Id { get; }
+        public string Lastname { get; }
+        public string Firstname { get; }
+        public string City { get; set; }
+
+        
+        [JsonIgnore] 
+        public SchoolClass SchoolClass { get; internal set; }
+
+        public Student(int id, string firstname, string lastname, string city)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            City = city;
+        }
+
+        public void ChangeClass(SchoolClass newClass)
+        {
+            if (SchoolClass != null)
+            {
+                SchoolClass.RemoveStudent(this);
+            }
+            newClass.AddStudent(this);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, SchoolClass> classes = new();
+            classes.Add("3AHIF", new SchoolClass(name: "3AHIF", classTeacher: "KV1"));
+            classes.Add("3BHIF", new SchoolClass(name: "3BHIF", classTeacher: "KV2"));
+            classes.Add("3CHIF", new SchoolClass(name: "3CHIF", classTeacher: "KV3"));
+
+            classes["3AHIF"].AddStudent(new Student(id: 1001, firstname: "FN1", lastname: "LN1", city: "CTY1"));
+            classes["3AHIF"].AddStudent(new Student(id: 1002, firstname: "FN2", lastname: "LN2", city: "CTY1"));
+            classes["3AHIF"].AddStudent(new Student(id: 1003, firstname: "FN3", lastname: "LN3", city: "CTY2"));
+            classes["3BHIF"].AddStudent(new Student(id: 1011, firstname: "FN4", lastname: "LN4", city: "CTY1"));
+            classes["3BHIF"].AddStudent(new Student(id: 1012, firstname: "FN5", lastname: "LN5", city: "CTY1"));
+            classes["3BHIF"].AddStudent(new Student(id: 1013, firstname: "FN6", lastname: "LN6", city: "CTY1"));
+
+            Student s = classes["3AHIF"].Students[0];
+            Console.WriteLine($"s sitzt in der Klasse {s.SchoolClass?.Name} mit dem KV {s.SchoolClass?.ClassTeacher}.");
+            Console.WriteLine($"In der 3AHIF sind folgende Städte: {JsonSerializer.Serialize(classes["3AHIF"].Cities)}.");
+
+            Console.WriteLine("3AHIF vor ChangeKlasse:");
+            Console.WriteLine(JsonSerializer.Serialize(classes["3AHIF"].Students));
+            s.ChangeClass(classes["3BHIF"]);
+            Console.WriteLine("3AHIF nach ChangeKlasse:");
+            Console.WriteLine(JsonSerializer.Serialize(classes["3AHIF"].Students));
+            Console.WriteLine("3BHIF nach ChangeKlasse:");
+            Console.WriteLine(JsonSerializer.Serialize(classes["3BHIF"].Students));
+            Console.WriteLine($"s sitzt in der Klasse {s.SchoolClass?.Name} mit dem KV {s.SchoolClass?.ClassTeacher}.");
+        }
+    }
+}
---
+++ referenceTypes/referenceTypes/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace PropertiesDemo.Application
+{
+   
+        public class Rectangle
+        {
+            public int Width { get; private set; }
+            public int Height { get; private set; }
+
+            public Rectangle(int width, int height)
+            {
+                if (width < 0 || height < 0)
+                {
+                    throw new ArgumentException("Ungültige Länge!!");
+                }
+
+                Width = width;
+                Height = height;
+            }
+
+            public int Area => Width * Height;
+
+            public void Scale(int factor)
+            {
+                if (factor <= 0)
+                {
+                    throw new ArgumentException("Faktor ist kleiner als 0!!");
+                }
+
+                Width *= factor;
+                Height *= factor;
+            }
+        }
+
+
+        class Teacher
+    {
+        public string Firstname { get; }
+        public string Lastname { get; }
+        public string Email { get; set; }
+        private decimal? salary = null;
+
+        public Teacher(string firstname, string lastname)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+
+        public string Longname => $"{Firstname} {Lastname}";
+
+        public string Shortname => Lastname.Substring(0, Math.Min(3, Lastname.Length)).ToUpper();
+
+        public bool IsSchoolEmail => !string.IsNullOrEmpty(Email) && Email.EndsWith("@spengergasse.at");
+
+        public decimal NetSalary => (salary ?? 0.0M) * 0.8M;
+
+        public decimal? Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (salary == null)
+                {
+                    salary = value;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        // DON'T TOUCH!
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("TESTS FÜR RECTANGLE");
+            Console.WriteLine("********************************************************************************");
+            if (typeof(Rectangle).GetConstructor(Type.EmptyTypes) is null) { Console.WriteLine("1 Kein default Konstruktor OK"); }
+            Rectangle rect = new Rectangle(width: 10, height: 20);
+            if (rect.Area == 200) { Console.WriteLine("2 Area OK"); }
+            rect.Scale(2);
+            if (rect.Area == 800) { Console.WriteLine("3 Scale OK"); }
+
+            if (typeof(Rectangle).GetProperty(nameof(Rectangle.Width))?.SetMethod?.IsPublic == false
+                && typeof(Rectangle).GetProperty(nameof(Rectangle.Height))?.SetMethod?.IsPublic == false)
+            {
+                Console.WriteLine("4 Kein Setzen der Breite und Höhe: OK");
+            }
+            try
+            {
+                Rectangle rect2 = new Rectangle(width: -1, height: 20);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("5 Exception bei negativer Breite OK");
+            }
+            try
+            {
+                Rectangle rect2 = new Rectangle(width: 10, height: -1);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("6 Exception bei negativer Höhe OK");
+            }
+            try
+            {
+                rect.Scale(-1);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("7 Exception bei Scale OK");
+            }
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("TESTS FÜR TEACHER");
+            Console.WriteLine("********************************************************************************");
+            if (typeof(Teacher).GetConstructor(Type.EmptyTypes) is null) { Console.WriteLine("1 Kein default Konstruktor OK"); }
+            if (typeof(Teacher).GetProperty(nameof(Teacher.Firstname))?.CanWrite == false
+                && typeof(Teacher).GetProperty(nameof(Teacher.Lastname))?.CanWrite == false)
+            {
+                Console.WriteLine("2 Vor- und Zuname sind immutable: OK");
+            }
+
+            Teacher t1 = new Teacher(firstname: "Fn", lastname: "Ln");
+            Teacher t2 = new Teacher(firstname: "Fn", lastname: "Lastname") { Email = "test@spengergasse.at", Salary = 2000M };
+            if (typeof(Teacher).GetProperty(nameof(Teacher.Longname))?.CanWrite == false
+                && t1.Longname == "Fn Ln") { Console.WriteLine("3 Longname OK"); }
+            if (typeof(Teacher).GetProperty(nameof(Teacher.Shortname))?.CanWrite == false
+                && t1.Shortname == "LN" && t2.Shortname == "LAS") { Console.WriteLine("4 Shortname OK"); }
+            if (t1.NetSalary == 0 && t2.NetSalary == 1600) { Console.WriteLine("5 NetSalary OK"); }
+            t1.Salary = 1000;
+            t2.Salary = 1000;
+            if (t1.Salary == 1000 && t2.Salary == 2000) { Console.WriteLine("6 Salary OK"); }
+            if (typeof(Teacher).GetProperty(nameof(Teacher.IsSchoolEmail))?.CanWrite == false
+                && !t1.IsSchoolEmail && t2.IsSchoolEmail) { Console.WriteLine("7 IsSchoolEmail OK"); }
+        }
+    }
+}
